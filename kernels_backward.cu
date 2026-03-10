457f097e533e2ee8f3ff92d2121ef0d53efc49e3
crankyastrologer <ansh0verma@gmail.com>
2026-03-10 10:48:24 +0000

    finish backward kernels

--- kernels_backward.cu
+++ kernels_backward.cu
@@ -158,3 +158,81 @@ __global__ void gelu_backwards_kernel(
         d_in[i] = d_out[i]*(cdf + pdf);
 
     }}
+
+    __global__ void kernel_transpose_reverse_merge_heads_layer(const float *dK, const float*dV,const float*dQ, float *out,int seq_len,int n_embed
+    ,const int head_dim, const int n_head, const int batch_size)
+    {
+        int idx = blockIdx.x;
+        int max_idx = batch_size*seq_len*3*n_embed;
+        if (idx >= max_idx) return;
+        int b = idx/ (seq_len * 3 * n_embed);
+        int s = (idx/(3*n_embed))%seq_len;
+        int qkv_id = (idx / n_embed)%3;
+        int h = (idx%n_embed)/head_dim;
+        int d = idx % head_dim;
+        int input_idx = (b*n_head*seq_len*head_dim)+(h*seq_len*head_dim)+(s*head_dim) + d;
+        if (qkv_id == 0) {
+    out[idx] = dQ[input_idx];
+} else if (qkv_id == 1) {
+    out[idx] = dK[input_idx];
+} else {
+    out[idx] = dV[input_idx];
+}
+    }
+
+__global__ void bias_layer(float * in, float *out,const int batch_size, const int seq_len, const int n_embed)
+{
+    size_t idx = threadIdx.x +blockDim.x*blockIdx.x;
+    size_t stride = n_embed;
+    if (idx >= n_embed) return;
+    int N = batch_size*seq_len*n_embed;
+    float local_sum = 0.0f;
+    for (size_t i = idx; i < N; i += stride) {
+        
+       local_sum+=in[i];
+    }
+    out[idx] = local_sum;
+} 
+
+__global__ void my_adamw_kernel(
+    float* params,          // The weights (p)
+    const float* grads,     // The gradients (g)
+    float* m_memory,        // First moment memory (m)
+    float* v_memory,        // Second moment memory (v)
+    const float lr,         // Learning rate
+    const float beta1,      
+    const float beta2,      
+    const float eps,        
+    const float weight_decay, 
+    const float bias_correction_1, // Precalculated (1 - beta1^t) on CPU
+    const float bias_correction_2, // Precalculated (1 - beta2^t) on CPU
+    const size_t N                 // Total number of elements
+){
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t stride = blockDim.x * gridDim.x;
+    for(size_t i = idx; i<N;i+=stride)
+    {
+        float p = params[i];
+        float g = grads[i];
+        float m = m_memory[i];
+        float v = v_memory[i];
+         m = beta1*m + (1-beta1)*g;
+         v = beta2*v +(1-beta2)*g*g;
+         float b_m = m/bias_correction_1;
+         float b_v = v/bias_correction_2;
+        float step = b_m/(sqrtf(b_v)+eps)+weight_decay*p;
+        p = p-(lr*step);
+        params[i] = p;
+        m_memory[i] = m;
+        v_memory[i] = v;
+    }
+
+}
+
+__global__ void zero_grad_kernel(float * in,const int size)
+{
+ size_t idx = blockIdx.x*blockDim.x + threadIdx.x;
+ if(idx>=size)
+    return;
+  in[idx] = 0;
+}
